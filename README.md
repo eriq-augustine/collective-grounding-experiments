659ba383c2fbd4b2451c04d33adb049f862aae78
Eriq Augustine <eriq.augustine@gmail.com>
2021-07-12 11:28:56 -0700

    Moving out old things for a new start.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Grounding experiments that work directly on the psl-examples repository.
+
+## System Setup
+
+Those who want to run the experiments will have to be sure that they can clear their postgres/system caches without manual intervention.
+The easiest way is probably adding an exception in your sudoers file (assuming `wheel` is the sudoers group):
+```
+%wheel ALL=(ALL) NOPASSWD: /path/to/repo/psl-grounding-experiments/scripts/clear_cache.sh
+```
--- scripts/formatting/get_all_summarized_fsse.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo -e "Experiment\tMisrank\tBase Time\tOptimal Time\tChosen Time"
-echo -e "Friendship\t$(./scripts/summarize_fsse_results.rb results/individual-query-*/friendship/all.out | tail -n +2)"
-echo -e "Trust\t$(./scripts/summarize_fsse_results.rb results/individual-query-*/trust-prediction/all.out | tail -n +2)"
-echo -e "ER\t$(./scripts/summarize_fsse_results.rb results/individual-query-*/entity-resolution/all.out | tail -n +2)"
